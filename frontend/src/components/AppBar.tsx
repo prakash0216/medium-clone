10ee1ca2a92de9115200ba06db6bd564b2ecd43b
prakash <pnagonde2002@gmail.com>
2024-07-19 11:14:41 +0530

    added logout functionality

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,7 +1,26 @@
 import { Avatar } from "./BlogCard";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const Appbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/signin");
+  };
+
   return (
     <div className="border-b flex justify-between px-10 py-4">
       <Link
@@ -10,16 +29,31 @@ export const Appbar = () => {
       >
         Medium
       </Link>
-      <div>
-        <Link to={`/publish`}>
-          <button
-            type="button"
-            className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
-          >
-            New
-          </button>
-        </Link>
-        <Avatar size={"big"} name="harkirat" />
+      <div className="flex">
+        <div>
+          <Link to={`/publish`}>
+            <button
+              type="button"
+              className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+            >
+              New
+            </button>
+          </Link>
+        </div>
+        <div>
+          {isLoggedIn ? (
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="mr-4 text-white bg-red-700 hover:bg-red-900 focus:outline-none focus:ring-4 focus:ring-red-900 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+            >
+              Log out
+            </button>
+          ) : null}
+        </div>
+        <div>
+          <Avatar size={"big"} name="harkirat" />
+        </div>
       </div>
     </div>
   );
